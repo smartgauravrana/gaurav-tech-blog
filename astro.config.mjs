c7c1a37ddaeae25b0a75011e56edd62e4793032b
Gaurav Rana <gaurav.ranaji96@gmail.com>
2024-08-31 20:44:08 +0530

    formatted some content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap()],
-  output: 'server',
+  output: "server",
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
-    }
+      enabled: true,
+    },
   }),
 });
